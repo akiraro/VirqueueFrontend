7375491a278087f751613e8debdd9734712e796d
unknown <emir7218@gmail.com>
2018-11-10 01:25:44 +0800

    initial push + header

---
+++ src/components/Header.js
@@ -0,0 +1,103 @@
+import React, { Component } from "react";
+
+import { withStyles } from "@material-ui/core/styles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import Hidden from "@material-ui/core/Hidden";
+
+import "../App.css";
+
+const styles = theme => ({
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    width: "100vw",
+    height: "100vh",
+    alignItems: "flex-end",
+    justifyContent: "center",
+    [theme.breakpoints.down("sm")]: {
+      alignItems: "center"
+    }
+  },
+  content: {
+    width: "400px",
+    marginRight: "10%",
+    [theme.breakpoints.down("sm")]: {
+      margin: 0
+    },
+    [theme.breakpoints.down("xs")]: {
+      width: "80%"
+    }
+  },
+  title: {
+    textAlign: "center",
+    textTransform: "uppercase",
+    letterSpacing: "10px",
+    color: theme.palette.primary.main,
+    marginBottom: "1rem"
+  },
+  text: {
+    textAlign: "justify",
+    [theme.breakpoints.down("sm")]: {
+      textAlign: "center"
+    }
+  },
+  buttonContainer: {
+    display: "flex",
+    marginTop: "2rem",
+    [theme.breakpoints.down("sm")]: {
+      justifyContent: "center"
+    }
+  },
+  button: {
+    width: "150px",
+    marginRight: "2rem",
+    [theme.breakpoints.down("sm")]: {
+      margin: "0 1rem"
+    }
+  }
+});
+
+class Header extends Component {
+  render() {
+    const { classes } = this.props;
+    return (
+      <div className={classes.root} id="header">
+        <div className={classes.content}>
+          <Hidden xsDown>
+            <Typography variant="h2" className={classes.title}>
+              virqueue
+            </Typography>
+          </Hidden>
+          <Hidden smUp>
+            <Typography variant="h4" className={classes.title}>
+              virqueue
+            </Typography>
+          </Hidden>
+          <Typography variant="body1" className={classes.text}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </Typography>
+          <div className={classes.buttonContainer}>
+            <Button
+              className={classes.button}
+              variant="contained"
+              color="primary"
+            >
+              i'm in
+            </Button>
+            <Button
+              className={classes.button}
+              variant="outlined"
+              color="primary"
+            >
+              learn more
+            </Button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(Header);
---
+++ src/theme.js
@@ -0,0 +1,42 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+export const theme = createMuiTheme({
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: ["Montserrat", "sans - serif"],
+    useNextVariants: true
+  },
+  palette: {
+    primary: {
+      light: "#4ca5b7",
+      main: "#007788",
+      dark: "#004a5a",
+      contrastText: "#ffffff"
+    },
+    secondary: {
+      light: "#ffffff",
+      main: "#ffffff",
+      dark: "#cccccc",
+      contrastText: "#000000"
+    }
+  },
+  overrides: {
+    MuiButton: {
+      containedPrimary: {
+        boxShadow: "none",
+        "&:hover": {
+          backgroundColor: "rgba(0,0,0,0)",
+          border: "1px solid #007788",
+          color: "#007788"
+        }
+      },
+      outlinedPrimary: {
+        border: "1px solid #007788",
+        "&:hover": {
+          backgroundColor: "#007788",
+          color: "#fff"
+        }
+      }
+    }
+  }
+});
